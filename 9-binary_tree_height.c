d9ae29798cd7e195fc75c60e4f998e9003265c95
anas-hfd <anas1.hfd@gmail.com>
2023-08-31 10:25:36 -0700

    height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: ptr to the root node
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	return (left >= right ? left + 1 : right + 1);
+}
